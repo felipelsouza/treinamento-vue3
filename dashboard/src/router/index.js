a588ecafda93639aef74e374a4e96ab4c9c9b93c
Felipe Souza <felipelsouza.dev@gmail.com>
2021-01-24 16:34:51 -0300

    Initial commit

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-const Home = () => import('../views/Home/index.vue')
-const Feedbacks = () => import('../views/Feedbacks/index.vue')
-const Credencials = () => import('../views/Credencials/index.vue')
+const Home = () => import('../views/Home/index.vue');
+const Feedbacks = () => import('../views/Feedbacks/index.vue');
+const Credencials = () => import('../views/Credencials/index.vue');
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/feedbacks',
-    name: 'Feedbacks',
+    path: "/feedbacks",
+    name: "Feedbacks",
     component: Feedbacks,
     meta: {
       hasAuth: true
     }
   },
   {
-    path: '/credencials',
-    name: 'Credencials',
+    path: "/credencials",
+    name: "Credencials",
     component: Credencials,
     meta: {
       hasAuth: true
@@ -30,11 +30,11 @@ const routes = [
     path: '/:pathMatch(.*)*',
     redirect: { name: 'Home' }
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory('/'),
   routes
-})
+});
 
-export default router
+export default router;
